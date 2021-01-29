58a71f77771f91bd074c278c6195156bdbbeb2cf
Aramosgr <aramosgr@gmail.com>
2021-01-29 17:54:08 +0000

    initial commit

---
+++ data.js
@@ -0,0 +1,67 @@
+
+module.exports = {
+    getPlayers: () => {
+        return [
+            {
+                name: "Tammy",
+                lastname: "Abraham",
+                position: "forward",
+                scoringChance: 45
+            },
+            {
+                name: "Florin",
+                lastname: "Andone",
+                position: "forward",
+                scoringChance: 32
+            },
+            {
+                name: "Andre",
+                lastname: "Gomes",
+                position: "midfielder",
+                scoringChance: 18
+            },
+            {
+                name: "Pierre-Emerick",
+                lastname: "Aubameyang",
+                position: "forward",
+                scoringChance: 61
+            },
+            {
+                name: "Ethan",
+                lastname: "Laird",
+                position: "defender",
+                scoringChance: 6
+            },
+            {
+                name: "Timo",
+                lastname: "Werner",
+                position: "forward",
+                scoringChance: 52
+            },
+            {
+                name: "Erik",
+                lastname: "Lamela",
+                position: "midfielder",
+                scoringChance: 21
+            },
+            {
+                name: "Bernd",
+                lastname: "Leno",
+                position: "Goalkeeper",
+                scoringChance: 1
+            },
+            {
+                name: "Diego",
+                lastname: "llorente",
+                position: "defender",
+                scoringChance: "4"
+            },
+            {
+                name: "Lucas",
+                lastname: "Moura",
+                position: "forward",
+                scoringChance: 28
+            }
+        ]
+    }
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+// We import the object from the data file. Inside that object there is a function to get players data
+const data = require("./data");
+
+/**
+ * Test 1
+ * Write a function to log in the console the players data with this format:
+ * PLAYER 1
+ * NAME: Zinedine
+ * LASTNAME: Zidane
+ * POSITION: Midfielder
+ * PLAYER 2...
+ */
+
+ // Your code
+
+
+
+/**
+ * Test 2
+ * Write a function to log in the console an array with only the names of the players, ordered A-Z
+ */
+
+// Your code
+
+
+
+
+
+/**
+ * Test 3
+ * Write a function to log in the console the average number of goals the players will score (assuming all of them play in a match)
+ * scoringChance means how many goals per 100 matches the player will score
+ * 4 means: 4 goals per 100 matches
+ * Output example -> Goals per match: 2.61
+ */
+
+// Your code
+
+
+
+/**
+ * Test 4
+ * Write a function that accepts a name, and logs the position of the player with that name
+ */
+
+// Your code
