c0c24a4376aabe1c61062062c2f84409df72fec6
Aramosgr <aramosgr@gmail.com>
2021-02-26 11:08:24 +0000

    small amend to the react test

--- reactTest.tsx
+++ reactTest.tsx
@@ -2,17 +2,18 @@
  * If you want to prove your React skills, please try this test
  * TestComponent is a React component
  * getDataFromExternalSource(string id) is a function and it can return an IData object or undefined
+ * The id comes from a reducer and the value may change at any time
  * 
  * 
  * WHAT THE COMPONENT SHOULD RETURN:
  * - If myData.age is less than 18, it should render the ComponentA
  * - If myData.age is greater than or equal to 18, it should render the ComponentB
+ * - If there is no data, it should display an error message "There was an error"
  * 
  * 
  * TIPS: 
  * - This is a theoretical question, there is no need to install packages.
  * - ComponentA and ComponentB are not real components, just an example. You won´t be able to render this.
- * - The id in the props could change.
  * - The external data function can return undefined
  */
 
@@ -20,21 +21,23 @@ import React from "react";
 import ComponentA from 'components/ComponentA';
 import ComponentB from 'components/ComponentB';
 
+interface IInitialState {
+    id: string;
+}
+
 const getDataFromExternalSource = (id: string): IData | undefined => {
     // This returns an IData object or undefined
 };
-
-export interface ITestComponentProps {
-    id: string;
-}
 
 interface IData {
     name: string;
     age: number;
 }
 
-const TestComponent = (props: ITestComponentProps) => {
+const TestComponent = () => {
+    const myReducer: IInitialState = useSelector((state: IRootState) => state.MyReducer);
 
+    
 }
 
 export default TestComponent;
