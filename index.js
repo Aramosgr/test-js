31459d0d0b74dba020d5200b3b34578a3469ce8b
Aramosgr <aramosgr@gmail.com>
2021-07-14 14:07:52 +0000

    Including test 5

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const data = require("./data");
 
 /**
  * Test 2
- * Write a function to log in the console an array with only the names of the players, ordered A-Z
+ * Write a function to log in the console an array with only the names of the players, ordered by name length descending
  */
 
 // Your code
@@ -31,7 +31,7 @@ const data = require("./data");
  * Write a function to log in the console the average number of goals there will be in a match if all the players in the data play on it
  * scoringChance means how many goals per 100 matches the player will score
  * Example: 10 players play in a match, all of them has 0.11 scoringChance, the result will be 1.1 average goals 
- * Output example -> Goals per match: 2.61
+ * Output example -> Goals per match: 2.19
  */
 
 // Your code
@@ -41,6 +41,20 @@ const data = require("./data");
 /**
  * Test 4
  * Write a function that accepts a name, and logs the position of the player with that name
+ */
+
+// Your code
+
+
+
+/**
+ * Test 5
+ * Write a function that splits all the players randomly into 2 teams, Team A and Team B. Both teams should have same number of players.
+ * The function should log the match score, using the average number of goals like the Test 3 and rounding to the closest integer
+ * Example:
+ *      Team A has 4 players, 2 with 50 scoringChance and 2 with 70 scoringChance. 
+ *      The average score for the team would be 2.4 (50+50+70+70 / 100), and the closest integer is 2, so the Team A score is 2
+ * Bonus points if you create a reusable function in the test 3 and you use it here
  */
 
 // Your code
