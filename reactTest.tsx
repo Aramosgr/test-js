03ae12c981a7f0ec1170a43c44a4d4168dc79be6
Aramosgr <aramosgr@gmail.com>
2021-09-17 10:19:05 +0000

    redoing the react test

--- reactTest.tsx
+++ reactTest.tsx
@@ -1,46 +1,72 @@
 /**
- * If you want to prove your React skills, please try this test
- * TestComponent is a React component
- * getDataFromExternalSource(string id) is a function and it can return an IData object or undefined
- * The id comes from a reducer and the value may change at any time
- * 
- * 
- * WHAT THE COMPONENT SHOULD RETURN:
- * - If myData.age is less than 18, it should render the ComponentA
- * - If myData.age is greater than or equal to 18, it should render the ComponentB
- * - If there is no data, it should display an error message "There was an error"
- * 
+ * If you want to prove your React skills, please try this test.
  * 
  * TIPS: 
- * - This is a theoretical question, there is no need to install packages.
- * - ComponentA and ComponentB are not real components, just an example. You won´t be able to render this.
- * - The external data function can return undefined
+ * - These are a theoretical questions, there is no need to install packages or run the code.
  */
 
 import React from "react";
-import ComponentA from 'components/ComponentA';
-import ComponentB from 'components/ComponentB';
 
-interface IInitialState {
-    id: string;
-}
+/**
+ * TEST 1
+ * 
+ * This test is a simple one with conditional rendering.
+ * 
+ * This component has to return:
+ *  The name in blue if the age is greater than or equal than 18.
+ *  The name in red otherwise.
+ */
 
-const getDataFromExternalSource = (id: string): IData | undefined => {
-    // This returns an IData object or undefined
-};
-
-interface IData {
+interface ITest1ComponentProps {
     name: string;
-    age: number;
+    age:number;
 }
 
-const TestComponent = () => {
-    const myReducer: IInitialState = useSelector((state: IRootState) => state.MyReducer);
+export const Test1Component = (props: ITest1ComponentProps) => {
 
+}
+
+
+/**
+ * TEST 2
+ * 
+ * This is test is about handling changes of the data from an API.
+ * 
+ * Like test 1, this component has to return:
+ *  The name in blue if the age is greater than or equal than 18.
+ *  The name in red otherwise.
+ * 
+ * The difference is we don't have the age, we need to use the function below to get it.
+ * The name from the parent can change any time, we have ensure the component rerenders if it happens
+ * Getting data from an API (we simulate it with a timeout) is async, please be sure the code updates when we get the response back from the API
+ */
+
+
+/**
+ * This function accepts a name and simulates and API call to get the age of the person
+ * Please use it in the component
+ * @param name Name of the person we want to find the age
+ * @returns random integer from 0 to 39
+ */
+const getAge = async (name: string): Promise<number>  => {
+    // This function calls an API and returns
+    return new Promise((resolve, reject) => {
+        return setTimeout(() => {
+            resolve(Math.floor(Math.random() * 40));
+        },500);
+    })
+}
+
+interface ITest2ComponentProps {
+    name: string;
+}
+
+export const Test2Component = (props: ITest2ComponentProps) => {
+
+    useEffect(() => {
+    }, []);
     
 }
-
-export default TestComponent;
 
 
 
